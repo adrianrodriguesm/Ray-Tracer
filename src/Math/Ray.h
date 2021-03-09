6bf9e35521456c7e05d4159484483a57be1e476b
Adrian <adrianrodriguesm98@gmail.com>
2021-03-09 07:38:21 +0000

    Small changed.

--- src/Math/Ray.h
+++ src/Math/Ray.h
@@ -25,43 +25,7 @@ namespace rayTracer
 		operator bool() { return Hit; }
 
 		static Ray CalculateRefractedRay(const RayCastHit& hit, const Ray& ray, float incidentRefractionIndex, float materialRefractionIndex);
-		/** /
-		Ray calculateRefractedRay(Hit hit, Ray ray, Material* mat, float RefractionIndex) {
-			Vector3 Nrefr = hit.Normal.normalize();
-			Vector3 I = (hit.Location - ray.O).normalize();
-			float NdotI = Nrefr * I;
-			NdotI = MathUtils::clamp(NdotI, 1, -1);
-			float etai = RefractionIndex, etat = mat->refractionIndex;
-			if (NdotI < 0) {
-				//outside the surface, cos theta should be positive
-				NdotI = -NdotI;
-			}
-			else {
-				//inside the surface, reverse normal direction
-				Nrefr = -hit.Normal.normalize();
-				std::swap(etai, etat);
-			}
-
-			float eta = etai / etat;
-			float k = 1 - ((eta * eta) * (1 - NdotI * NdotI));
-			if (k < 0) {
-				return Ray(hit.Location, Vector3(0, 0, 0));
-			}
-			else
-				return Ray(hit.Location, eta * (I)+(eta * NdotI - sqrtf(k)) * Nrefr);
-
-		}
-		/**/
 		// View Dir Need to be normalize
 		static Ray CalculateReflectedRay(const RayCastHit& hit, const Vec3& dir);
-		/** /
-		Ray CalculateReflectedRay(RayCastHit& hit, Vec3& viewPosition)
-		{
-			Vec3 viewDir = Vec3(hit.InterceptionPoint - viewPosition).Normalized();
-			Vec3 normal = hit.Object->GetNormal(hit.InterceptionPoint);
-			Vec3 reflectionDir = Reflect(-viewDir, normal);
-			return { hit.InterceptionPoint , reflectionDir };
-		}
-		/**/
 	};
 }
